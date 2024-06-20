3847c98811f182394b7d850ceceb3519d09971aa
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-06-19 23:15:03 -0600

    Create Conexion, Helpers and Repository class

---
+++ Biblioteca/Biblioteca/Clases/Conexion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biblioteca.Clases
+{
+    public class Conexion
+    {
+        //Cadena de Conexion -- > Instancia
+        public static string cadena_de_conexion;
+        //public static string cadena_de_conexion = @"Server=" + Env.SERVER + ";Database=" + Env.DATABASE + ";User Id=" + Env.USERID + ";Password=" + Env.PASSWORD + ";";
+        public static SqlConnection ConSql = new SqlConnection(cadena_de_conexion);
+
+        //Metodo OpenConnection --> Abre la conexion a la base de datos
+        public static void OpenConnection()
+        {
+            try
+            {
+                ConSql.Open();
+            }
+
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        } //Fin Metodo
+
+        //Metodo CloseConnection --> Cierra la conexion a la base de datos
+        public static void CloseConnection()
+        {
+            try
+            {
+                ConSql.Close();
+            }
+
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        } //Fin Metodo
+
+        //Metodo EndsConnection --> Revisa que la conexion a SQL este cerrada
+        public static void EndsConnection()
+        {
+            if (ConSql.State == ConnectionState.Open)
+            {
+                ConSql.Close();
+            }
+        } //Fin Metodo
+    }
+}
---
+++ Biblioteca/Biblioteca/Clases/Repository.cs
@@ -0,0 +1,156 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Biblioteca.Clases
+{
+    public class Repository
+    {
+        private Clases.Helpers h = new Clases.Helpers();
+        public SqlCommand com;
+        private SqlDataReader reader;
+        private DataTable recordset;
+        private string query;
+
+        //Metodo Save
+        //inserta registros en la base de datos
+        public int Save(string table, string fields, string values, string debug = "")
+        {
+            int ra = 0; //rowsaffected
+            query = "INSERT INTO " + table + "(" + fields + ") VALUES(" + values + ")";
+
+            if (debug == "True")
+            {
+                MessageBox.Show(query);
+            }
+
+            try
+            {
+                com = new SqlCommand(query, Clases.Conexion.ConSql);
+                Clases.Conexion.OpenConnection();
+
+                ra = com.ExecuteNonQuery();
+
+                com.Dispose();
+                Clases.Conexion.CloseConnection();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally
+            {
+                Clases.Conexion.EndsConnection();
+            }
+
+            return ra;
+        }
+        //fin Metodo Save
+
+        //Metodo Update
+        //Actualizar los registros de la base de datos
+        public int Update(string table, string values, string condition = "", string debug = "")
+        {
+            int ra = 0; //rowsaffected
+            if (condition == "")
+            {
+                query = "UPDATE " + table + " SET " + values;
+            }
+            else
+            {
+                query = "UPDATE " + table + " SET " + values + " WHERE " + condition;
+            }
+
+            if (debug == "True")
+            {
+                MessageBox.Show(query);
+            }
+
+            try
+            {
+                com = new SqlCommand(query, Clases.Conexion.ConSql);
+                Clases.Conexion.OpenConnection();
+
+                ra = com.ExecuteNonQuery();
+
+                com.Dispose();
+                Clases.Conexion.CloseConnection();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally
+            {
+                Clases.Conexion.EndsConnection();
+            }
+            return ra;
+        }
+        //Fin Metodo Update
+
+        //Metodo Destroy
+        //Elimina registros de la base de datos
+        public int Destroy(string table, string condition = "")
+        {
+            int ra = 0;
+            if (condition == "")
+            {
+                query = "DELETE FROM " + table;
+            }
+            else
+            {
+                query = "DELETE FROM " + table + " WHERE " + condition;
+            }
+
+            try
+            {
+                com = new SqlCommand(query, Clases.Conexion.ConSql);
+                Clases.Conexion.OpenConnection();
+
+                ra = com.ExecuteNonQuery();
+
+                com.Dispose();
+                Clases.Conexion.CloseConnection();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally
+            {
+                Clases.Conexion.EndsConnection();
+            }
+            return ra;
+        }
+        //Fin Metodo Destroy
+
+        //Metodo Delete
+        //Marca como eliminado el registro seleccionado
+        public int Delete(string table, string pk, string value)
+        {
+            int rd = 0;
+            try
+            {
+                query = "UPDATE " + table + " SET DEL='S' WHERE " + pk + "='" + value + "'";
+                com = new SqlCommand(query, Clases.Conexion.ConSql);
+                Clases.Conexion.OpenConnection();
+
+                rd = com.ExecuteNonQuery();
+
+                com.Dispose();
+                Clases.Conexion.CloseConnection();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally
+            {
+                Clases.Conexion.EndsConnection();
+            }
+
+            return rd;
+        }
+        //Fin Delete
+    }
+}
